61293d2bac08e27811f04b0f13fd39f4f9857478
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-03 17:00:54 -0300

    Primeiro exercício  de selenium e junit 5

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/PrimeiraExecucao.java
@@ -0,0 +1,19 @@
+package br.com.andre_dev_ALS.cursoSelenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+;
+
+public class PrimeiraExecucao {
+
+	@Test
+	public void testeGoogle() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+
+		Assert.assertEquals("Google", driver.getTitle());
+	}
+}
