91eb040314a20dd5abb11a4cdf68f2e1a3d07d9a
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-04 14:34:06 -0300

    manipulando a posição e o tamanho da tela do navegador

--- cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/PosicaoeTamanhoTela.java
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/PosicaoeTamanhoTela.java
@@ -1,17 +1,19 @@
 package br.com.andre_dev_ALS.cursoSelenium;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class PrimeiraExecucao {
+public class PosicaoeTamanhoTela {
 
 	@Test
-	public void testeGoogle() {
+	public void teste() {
 		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.manage().window().maximize();
 		driver.get("http://www.google.com");
-
 		Assert.assertEquals("Google", driver.getTitle());
 	}
+
 }
