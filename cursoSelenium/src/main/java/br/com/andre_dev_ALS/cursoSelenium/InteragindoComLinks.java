e49507f9d11db11476c42bce2ca4bdf079249d9f
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-09 19:41:08 -0300

    interagindo com links

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/InteragindoComLinks.java
@@ -0,0 +1,19 @@
+package br.com.andre_dev_ALS.cursoSelenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteragindoComLinks {
+@Test
+
+public void interagindoComLink() {
+	WebDriver  driver  = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.findElement(By.linkText("Voltar")).click();
+	
+	driver.quit();
+}
+}
