49588669afad6973e819ea3613c5cd5acfd2f002
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-10 01:14:41 -0300

    interagindo com o prompt

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/InteragindoComPrompt.java
@@ -0,0 +1,28 @@
+package br.com.andre_dev_ALS.cursoSelenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteragindoComPrompt {
+	@Test
+	public void interagirComPrompt() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("prompt")).click();
+		Alert prompt = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", prompt.getText());
+		prompt.sendKeys("31");
+		prompt.accept();
+		Assert.assertEquals("Era 31?", prompt.getText());
+		prompt.accept();
+		Assert.assertEquals(":D", prompt.getText());
+
+		prompt.accept();
+		driver.quit();
+	}
+}
