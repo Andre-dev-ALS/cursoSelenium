77c1bb12e38ffe692efb1bdf045dad6f2fa2e39e
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-10 14:40:14 -0300

    interagindo com uma janela externa do navegador que não tem nome

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/InteragindoComJanelasSemNome.java
@@ -0,0 +1,21 @@
+package br.com.andre_dev_ALS.cursoSelenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteragindoComJanelasSemNome {
+@Test
+public void  interagirComJanelaSemNome() {
+	WebDriver driver  = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.findElement(By.id("buttonPopUpHard")).click();
+	driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+	driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+	driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+	driver.findElement(By.tagName("textarea")).sendKeys("E agora?");
+	driver.quit();
+}	
+}
