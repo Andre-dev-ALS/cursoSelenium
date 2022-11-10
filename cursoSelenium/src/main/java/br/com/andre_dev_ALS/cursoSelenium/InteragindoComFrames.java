cbf028296cb287059ebf82e92df42e0ae63c0a85
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-10 11:41:34 -0300

    interagindo com os frames

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/InteragindoComFrames.java
@@ -0,0 +1,26 @@
+package br.com.andre_dev_ALS.cursoSelenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InteragindoComFrames {
+@Test
+public void interagirComFrames() {
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.switchTo().frame("frame1");
+	driver.findElement(By.id("frameButton")).click();
+	Alert alerta = driver.switchTo().alert();
+Assert.assertEquals("Frame OK!", alerta.getText());
+	alerta.accept();
+	
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("elementosForm:nome")).sendKeys("Testando o campo");
+	driver.quit();
+}
+}
