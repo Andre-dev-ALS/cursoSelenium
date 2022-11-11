e0d853659a86ecd9896c8cc1ee08977b642492e4
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-11 17:29:48 -0300

    segunda versão do arquivo  interagindoComCaixasDeTextos

--- cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/fundamentoOrganizacaoDeCodigo/Dsl.java
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/fundamentoOrganizacaoDeCodigo/Dsl.java
@@ -1,13 +1,27 @@
 package br.com.andre_dev_ALS.cursoSelenium.fundamentoOrganizacaoDeCodigo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Dsl {
-private WebDriver driver;
+	private WebDriver driver;
 
-public Dsl(WebDriver driver) {
-	super();
-	this.driver = driver;
-}
+	public Dsl(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
 
+	// TextField e TextArea
 
+	public void escrever(By by, String texto) {
+		driver.findElement(by).sendKeys(texto);
+	}
 
+	public void escrever(String idCampo, String texto) {
+		escrever(By.id(idCampo), texto);
+	}
+
+	public String obterValorCampo(String idCampo) {
+		return driver.findElement(By.id(idCampo)).getAttribute("value");
+	}
 }
--- cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/fundamentoOrganizacaoDeCodigo/InteragirComCaixaDeEdicao.java
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/fundamentoOrganizacaoDeCodigo/InteragirComCaixaDeEdicao.java
@@ -1,46 +1,41 @@
 package br.com.andre_dev_ALS.cursoSelenium.fundamentoOrganizacaoDeCodigo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InteragirComCaixaDeEdicao {
-private WebDriver driver;
-
+WebDriver driver;
+Dsl dsl;
 
 @Before
 public void iniciar() {
-	WebDriver driver = new ChromeDriver();
+	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	dsl = new Dsl(driver);
 }
-@AfterAll
+
+@After
 public void finalizar() {
 	driver.quit();
 }
 
 	@Test
 	public void testeTextField() {
-		
 
-		
-
-		driver.findElement(By.id("elementosForm:nome")).sendKeys("andré");
-		Assert.assertEquals("andré", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		dsl.escrever("elementosForm:nome", "André");
+		assertEquals("André", dsl.obterValorCampo("elementosForm:nome"));
 	}
 
 	@Test
 	public void deveIntegarirComTextArea() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1200, 765));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste\n\naasldjdlks\nUltima linha");
-		Assert.assertEquals("teste\n\naasldjdlks\nUltima linha",
-				driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		dsl.escrever("elementosForm:sugestoes", "teste\n\naasldjdlks\nUltima linha");
+		Assert.assertEquals("teste\n\naasldjdlks\nUltima linha", dsl.obterValorCampo("elementosForm:sugestoes"));
 	}
 }
