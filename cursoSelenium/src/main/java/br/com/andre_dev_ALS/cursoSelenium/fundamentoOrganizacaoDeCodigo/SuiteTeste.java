29e529eee23d3d3ed7c5af2db8e9e5fe21cf3ca2
Andre_dev_ALS <andreluizsilvasilva2019@gmail.com>
2022-11-18 22:27:34 -0300

    Suite de teste

---
+++ cursoSelenium/src/main/java/br/com/andre_dev_ALS/cursoSelenium/fundamentoOrganizacaoDeCodigo/SuiteTeste.java
@@ -0,0 +1,14 @@
+package br.com.andre_dev_ALS.cursoSelenium.fundamentoOrganizacaoDeCodigo;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	DesafioCadastro.class,
+	TestesParametrizáveis.class,
+	
+})
+public class SuiteTeste {
+
+}
